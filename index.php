1721b23fb0ec534bf88ee31c5dd41beb6b114c7d
devzer0 <devzer0@devzer0-H87M-D3H>
2013-12-20 17:50:12 +0700

    updated

--- index.php
+++ index.php
@@ -28,15 +28,13 @@ $app->get('/fbauth', function () use ($app, $smarty) {
 
 $app->get('/fblogin', function () use ($app, $smarty) {
 	
-	if($_SESSION['state'] && ($_SESSION['state'] !== $_REQUEST['state']))
-	{
+	if(isset($_SESSION['state']) && ($_SESSION['state'] !== $_REQUEST['state'])) {
 		//do something here
 		
 		return true;
 	}
 	
-	if($_REQUEST['error'])
-	{
+	if(isset($_REQUEST['error'])) {
 		//do something here
 		
 		return true;
@@ -61,7 +59,7 @@ $app->get('/fblogin', function () use ($app, $smarty) {
 	}
 	
 	$pdo = getDbHandler();
-	$sql = "SELECT username, password FROM member WHERE fb_id = :fb_id LIMIT 1"
+	$sql = "SELECT username, password FROM member WHERE fb_id = :fb_id LIMIT 1";
 	$sth = $pdo->prepare($sql);
 	$sth->execute(array(':fb_id' => $user->id));
 	
@@ -71,145 +69,27 @@ $app->get('/fblogin', function () use ($app, $smarty) {
 		$sth->execute(array(':fb_token' => $params['access_token'] , 'fb_id' => $user->id));
 		
 		//handle login and redirect
+		
+		return true;
 	}
 	
-				
-		$save = array(
-							"username" => $username,
-							"password" => $password,
-							"forname" => $user->first_name,
-							"surname" => $user->last_name,
-							"validation_code" => $validation_code,
-							"birthday" => date("Y-m-d", strtotime($user->birthday)),
-							"gender" => ($user->gender=="male")?1:2,
-							"email" => $user->email,
-							"facebook_id" => $user->id,
-							"facebook_username" => $user->username,
-							"facebook_token" => $params['access_token'],
-							"username_confirmed" => 0,
-							"isactive" => 0,
-							"type" => 4,
-							"signup_datetime" => funcs::getDateTime(),
-							"lookmen" => ($user->gender=="male")?"0":"1",
-							"lookwomen" => ($user->gender=="male")?"1":"0",
-							"description" => ($user->bio)?addslashes($user->bio):""
-		);
+	$image_url = "https://graph.facebook.com/".$user->id."/picture?type=large&access_token=" . $params['access_token'];
+	$fb_image = file_get_contents($image_url);
 	
-					if($user->location)
-					{
-						$countries = DBConnect::assoc_query_2D("SELECT id, name FROM xml_countries WHERE status=1");
-						foreach($countries as $country)
-						{
-							if(strpos($user->location->name, $country['name'])!==false)
-							{
-								$save['country'] = $country['id'];
-							}
-						}
 	
-						if($save['country'])
-						{
-							$states = DBConnect::assoc_query_2D("SELECT id, name FROM xml_states WHERE status=1 and parent=".$save['country']);
-							foreach($states as $state)
-							{
-								if(strpos($user->location->name, $state['name'])!==false)
-								{
-									$save['state'] = $state['id'];
-								}
-							}
+	$sql = "INSERT INTO user (username, password, email, dob, fb_id, fb_token, fb_image, fb_first_name, fb_last_name, fb_username, fb_gender, fb_bio, "
+	     . "                    fb_hometown_id, fb_hometown_name, fb_location_id, fb_location_name, fb_religion, created) "
+	     . " VALUES (:username, :password, :email, :dob, :fb_id, :fb_token, :fb_image, :fb_first_name, :fb_last_name, :fb_username, :fb_gender, :fb_bio, "
+		 . "                    :fb_hometown_id, :fb_hometown_name, :fb_location_id, :fb_location_name, :fb_religion, NOW()) ";
 	
-							if($save['state'])
-							{
-								$cities = DBConnect::assoc_query_2D("SELECT id, name FROM xml_cities WHERE status=1 and parent=".$save['state']);
-								foreach($cities as $city)
-								{
-									if(strpos($user->location->name, $city['name'])!==false)
-									{
-										$save['city'] = $city['id'];
-									}
-								}
-							}
-						}
-					}
-					elseif($user->hometown)
-					{
-						$countries = DBConnect::assoc_query_2D("SELECT id, name FROM xml_countries WHERE status=1");
-						foreach($countries as $country)
-						{
-							if(strpos($user->hometown->name, $country['name'])!==false)
-							{
-								$save['country'] = $country['id'];
-							}
-						}
+	$sth = $pdo->prepare($sql);
+	$sth->execute(array(
+				':username' => 'A', ':password' => 'B', ':email' => $user->email, 'dob' =>  date("Y-m-d", strtotime($user->birthday)),':fb_id' => $user->id, ':fb_token' => $params['access_token'],
+				':fb_image' => $fb_image, ':fb_first_name' => $user->first_name, ':fb_last_name' => $user->last_name, ':fb_username' => $user->username,
+				':fb_gender' => $user->gender, ':fb_bio' => $user->bio, ':fb_hometown_id' => $user->hometown->id, ':fb_hometown_name' => $user->hometown->name,
+				':fb_location_id' => $user->location->id, ':fb_location_name' => $user->location->name, ':fb_religion' => $user->religion));
 	
-						if($save['country'])
-						{
-							$states = DBConnect::assoc_query_2D("SELECT id, name FROM xml_states WHERE status=1 and parent=".$save['country']);
-							foreach($states as $state)
-							{
-								if(strpos($user->hometown->name, $state['name'])!==false)
-								{
-									$save['state'] = $state['id'];
-								}
-							}
-	
-							if($save['state'])
-							{
-								$cities = DBConnect::assoc_query_2D("SELECT id, name FROM xml_cities WHERE status=1 and parent=".$save['state']);
-								foreach($cities as $city)
-								{
-									if(strpos($user->hometown->name, $city['name'])!==false)
-									{
-										$save['citiy'] = $city['id'];
-									}
-								}
-							}
-						}
-					}
-					$colnames = array_flip(DBconnect::get_col_names('member'));
-					$member_post = array_intersect_key($save, $colnames);
-	
-					DBconnect::assoc_insert_1D($member_post, 'member');
-					$userid = mysql_insert_id();
-	
-					$image_url = "http://graph.facebook.com/".$user->id."/picture?type=large";
-					$headers = get_headers($image_url, 1);
-					$content_length = $headers["Content-Length"];
-					if($content_length>2048)
-					{
-						$uploaddir = UPLOAD_DIR.$userid.'/';
-						if(!is_dir($uploaddir))	//check have my user id directory
-							mkdir($uploaddir, 0777); //create my user id directory
-	
-						$filename = time().'.jpg';
-						copy($image_url, $uploaddir.$filename);
-						$picturepath = $userid."/".$filename;
-	
-						if(PHOTO_APPROVAL == 1){
-							funcs::updatePhotoToTemp($userid, $picturepath);
-						}
-					}
-	
-					$subject = funcs::getText($_SESSION['lang'], '$email_testmember_subject');	//get subject message
-					$message = funcs::getMessageEmail_Forgot($smarty,$username, $password, "facebook");	//get message
-					funcs::sendMail($user->email, $subject, $message, MAIL_FROM);
-	
-					$args=array(
-							'access_token' => $params['access_token'],
-							"name"=>$username." start using ".ucfirst($domain),
-							"picture"=>"http://www.".$domain."/sites/".$domain."/images/cm-theme/facebook-128.jpg",
-					);
-						
-					facebookPostOnUserWall($args);
-	
-					header("location: ". MY_URL."?action=activate&username=".$username."&password=".$password."&code=".$validation_code);
-				}
-			}
-			else
-			{
-				echo("Failed, facebook id and email are needed.");
-			}
-		}
-	}
+	return true;
 });
 
 $app->get('/notregister', function () use ($app, $smarty) {
