ba4167d6923f6cdc254b7f2c7713fd8ffa3113f2
devzer01 <nayana@corp-gems.com>
2013-12-20 18:00:52 +0700

    removed APP_SECRET

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ define('SLB_DBUSER', 'root');
 define('SLB_DBPASS', '');
 
 define('APP_ID', "386041674873265");
-define('APP_SECRET', "da597703633cc8cefb779636120ba6f1");
+define('APP_SECRET', "");
 define('MY_URL', "http://lab.slbeat.com:10045/fblogin");
 define('FACEBOOK_LOGIN_URL', 'https://www.facebook.com/dialog/oauth/?client_id='.APP_ID.'&redirect_uri='.MY_URL.'&scope=email,user_birthday,user_location,user_about_me,publish_actions&state=');
